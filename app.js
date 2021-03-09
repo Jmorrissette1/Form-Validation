281041b8fefc121673419b16e07b3f43b05b2f71
Jmorrissette1 <J.morrissette47@gmail.com>
2021-03-09 13:45:18 -0800

    Added Form Validation

---
+++ app.js
@@ -0,0 +1,25 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement  = document.getElementById('error');
+
+addEventListener('submit', (e) =>{
+    let messages = []
+    if(email.value === '' || email.value == null){
+        messages.push('Email is required');
+    }
+
+    if(password.value.length <= 6){
+        messages.push('Password must be longer than 6 characters');
+    }else if(password.value.length >= 20){
+        messages.push('Password must be shorter than 20 characters');
+    }else if(password.value === 'password'){
+        messages.push('Password Cannot be password.');
+    }
+
+    if(messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+    }
+  
+});
